5dcebc3a6ccb1c40095afee5cc4b0bf9a1a41bbf
sk <skaih@qq.com>
2017-01-07 16:14:08 +0800

    ee

--- src/ios/Skcoreplugin.h
+++ src/ios/Skcoreplugin.h
@@ -1,6 +1,15 @@
 /********* TestPlugin.h Cordova Plugin Header *******/
 
 #import <Cordova/CDV.h>
+#define loginToken @"loginToken"
 @interface Skcoreplugin : CDVPlugin
 - (void)test:(CDVInvokedUrlCommand*)command;
++(void)al:(NSString *) str;
++(void)alstr:(NSString *) str iv:(UIView *)v;
++(void)alint:(NSInteger ) i;
+
+ 
++(NSString *)getUserInfor:(NSString* )str;
++(NSString *)loginsave:(NSString* )str;
++(NSString *)getLoginToken;
     @end
--- src/ios/Skcoreplugin.m
+++ src/ios/Skcoreplugin.m
@@ -1,12 +1,52 @@
-﻿/***************** TestPlugin.m ********************/
+/***************** TestPlugin.m ********************/
 
 #import "Skcoreplugin.h"
 #import <Cordova/CDV.h>
-
-@implementation Skcoreplugin- (void)test:(CDVInvokedUrlCommand*)command
+#import "DCore.h"
+@implementation Skcoreplugin
+- (void)test:(CDVInvokedUrlCommand*)command
     {
         UIAlertView *alertview = [[UIAlertView alloc] initWithTitle:@"标题" message:@"你好世界！" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
         [alertview show];
     }
+
++(void)al:(NSString *) str{
+    [DCore al:str];
     
-    @end
+}
++(void)alstr:(NSString *) str iv:(UIView *)v{
+    
+    [DCore alstr:str iv:v];
+    
+}
++(void)alint:(NSInteger ) i{
+    [DCore alint:i];
+    
+}
+
++(NSString *)getUserInfor:(NSString* )str{
+    
+    return [DCore getUserInfor:str];
+}
+
++(NSString *)loginsave:(NSString* )str{
+    NSUserDefaults *standardDefaults = [NSUserDefaults standardUserDefaults];
+    [standardDefaults setObject:str forKey:loginToken];
+    [standardDefaults synchronize];
+    return @"";
+}
++(NSString *)getLoginToken{
+    NSUserDefaults *standardDefaults = [NSUserDefaults standardUserDefaults];
+    NSString *token = [standardDefaults stringForKey:loginToken];
+    
+    return token;
+}
++(NSInteger)getUserId{
+    
+    NSUserDefaults *standardDefaults = [NSUserDefaults standardUserDefaults];
+    NSDictionary *getDic=[standardDefaults dictionaryForKey:userNameKey];
+    NSInteger userId=[[getDic objectForKey:@"UserId"] integerValue];
+    [standardDefaults synchronize];
+    return userId;
+}
+@end
