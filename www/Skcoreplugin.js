47f02fef7bb9c5105bf4cf0e7b57c52bea73bde5
sk <skaih@qq.com>
2017-01-07 17:40:26 +0800

    3

--- www/Skcoreplugin.js
+++ www/Skcoreplugin.js
@@ -34,11 +34,11 @@ var exec = require('cordova/exec');
 cordova.define("cordova-plugin-Skcoreplugin", function(require, exports, module) { //第一个字符串表示你的插件的id，在cordova_plugins.js中需要用到
 var exec = require('cordova/exec');
 var locateCore =  {
-               ac:function(action,str,callback){ //js调用的方法
+               ac:function(callback){ //js调用的方法
 			        if(action!=null){action="login";}
                     exec(callback, function(err){
                     alert("网络异常");
-                    }, "Skcoreplugin", "test", [action,str]);
+                    }, "Skcoreplugin", "test", [""]);
 					
 		 }
                };
