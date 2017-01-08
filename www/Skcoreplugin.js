e9154fa7468fb35e83f6971ecfc138287bbbf568
sk <skaih@qq.com>
2017-01-08 14:46:07 +0800

    1

--- www/Skcoreplugin.js
+++ www/Skcoreplugin.js
@@ -1,36 +1,16 @@
-/*
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
-*/
-
-/* global cordova 
-
-var exec = require('cordova/exec');
- window.echo = function(str, callback) {
+ var exec = require('cordova/exec');
+ window.ac = function(str, callback) {
         cordova.exec(callback, function(err) {
             callback('Nothing to echo.');
-        }, "Sktest", "test", [str]);
+        }, "Skcoreplugin", "test", [action,str]);
  };
- window.echo("echome", function(echoValue) {
-        alert(echoValue == "echome"); // should alert true.
-    });
-*/
+ 
+/*window.ac("al", "", function(echoValue) {
+       alert(echoValue == "echome"); // should alert true.
+});
+*/ 
+
+/*
 cordova.define("cordova-plugin-Skcoreplugin", function(require, exports, module) { //第一个字符串表示你的插件的id，在cordova_plugins.js中需要用到
 var exec = require('cordova/exec');
 var locateCore =  {
@@ -46,6 +26,7 @@ var locateCore =  {
 module.exports = locateCore;
 
 });
+*/
 
 //window.Skcoreplugin.login();
 //window.Skcoreplugin.login("al", "",function(echoValue) {
